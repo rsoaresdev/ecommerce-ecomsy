f8d5164c33185bb6bdf24a3d31dca9728dcacddd
Rafael Soares <107703258+SirRamboia@users.noreply.github.com>
2023-11-22 20:25:53 +0000

    feat: Implement login and registration system with Clerk Auth
    Integrating Clerk Auth to enable user authentication functionalities in the ecomsy ecommerce website. This commit includes the implementation of the login and registration system, leveraging Clerk Auth for a secure and seamless authentication process.
    
    # Details:
    - Configured Clerk Auth for user authentication.
    - Implemented user registration functionality.
    - Integrated user login feature.
    
    This marks a significant milestone in the development of the ecomsy project, providing a foundation for user management and authentication.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="pt">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+
   )
 }
