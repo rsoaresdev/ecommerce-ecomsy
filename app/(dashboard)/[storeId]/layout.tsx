359c578c9f3b3e9630972e9997eb747c88ea293c
Rafael Soares <107703258+SirRamboia@users.noreply.github.com>
2023-11-24 23:01:33 +0000

    feat: Complete Navbar with store selection, quick access routes, and Clerk profile button
    Successfully finished the Navbar, incorporating essential features:
    - Implemented a Combobox for store selection, allowing users to easily switch between stores.
    - Added quick access links for efficient navigation to essential routes.
    - Integrated a Clerk profile button for seamless user access and management (<UserButton />).

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,6 +1,8 @@
-import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+
+import prismadb from "@/lib/prismadb";
+import Navbar from "@/components/navbar";
 
 export default async function DashboardLayout({
   children,
@@ -30,7 +32,7 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <div>This is will be a Navbar</div>
+      <Navbar />
       {children}
     </>
   );
