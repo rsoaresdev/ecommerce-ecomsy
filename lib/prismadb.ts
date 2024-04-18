2dc1a38439405b61bb1da4fb80a7b75a73727975
Rafael Soares <107703258+rsoaresdev@users.noreply.github.com>
2024-04-18 22:41:59 +0100

    fix: prismadb client initialization
    This change updates the Prisma configuration file to fix TypeScript typing errors and ensure proper initialization of the Prisma client.

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+
 declare global {
-  let prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
 }
 
-const prismadb = globalThis.prisma ?? new PrismaClient();
+const prismadb = globalThis.prisma || new PrismaClient();
+
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
 
 export default prismadb;
